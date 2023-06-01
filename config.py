991021dfdc93daa1334577c0a1cd7ac7fab52178
xeny <44981148+1x6@users.noreply.github.com>
2023-06-01 20:42:36 +0100

    add config vars

--- config.py
+++ config.py
@@ -1,11 +1,29 @@
+import os
+
 def token():
-    return 'zz'
+    env_token = os.environ.get('TOKEN')
+    if env_token:
+        return env_token
+    else:
+        return 'zz'
 
 def smtp_username():
-    return 'zz@gmail.com'
+    env_username = os.environ.get('SMTP_USERNAME')
+    if env_username:
+        return env_username
+    else:
+        return 'zz@gmail.com'
 
 def smtp_password():
-    return 'zz123'
+    env_password = os.environ.get('SMTP_PASSWORD')
+    if env_password:
+        return env_password
+    else:
+        return 'zz123'
 
 def telegram_bot_token():
-    return 'zz'
+    env_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
+    if env_bot_token:
+        return env_bot_token
+    else:
+        return 'zz'
